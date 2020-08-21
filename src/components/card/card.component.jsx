9655c1572e8217d5836eca3417c324f19cdd5c67
Keng Hong Ho <kenghongho@Kengs-MacBook-Pro.local>
2020-08-21 22:11:55 +0800

    first commit test01

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './card.styles.scss';
+
+export const Card = (props) => (
+    <div className='card-container'>
+        <img alt={props.monster.name} src={`https://robohash.org/${props.monster.id}?set=set3&size=180x180`} />
+        <h2 key={props.monster.id}> {props.monster.name} </h2>
+        <p>{props.monster.email}</p>
+    </div>
+)
