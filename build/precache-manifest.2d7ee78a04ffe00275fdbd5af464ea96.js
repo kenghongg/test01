639c2fa0e4b6dc0712d139b7e43b202c7abf44fd
Keng Hong Ho <kenghongho@Kengs-MacBook-Pro.local>
2020-08-21 22:17:32 +0800

    build done

---
+++ build/precache-manifest.2d7ee78a04ffe00275fdbd5af464ea96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed3aead770736aa6b30dd87fc8099c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67670fd311100af07377",
+    "url": "/static/css/main.9dfccf3e.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67670fd311100af07377",
+    "url": "/static/js/main.b342749e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
